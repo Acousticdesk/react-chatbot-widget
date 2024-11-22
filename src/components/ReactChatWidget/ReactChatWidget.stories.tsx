c2c4d24ea4cca6c65dda7f989793e477a49e9c48
Andrii Kicha <akicha@star.global>
2024-11-22 11:35:45 -0700

    remove the unnecessary dark theme component wrapper

--- src/components/ReactChatWidget/ReactChatWidget.stories.tsx
+++ src/components/ReactChatWidget/ReactChatWidget.stories.tsx
@@ -1,8 +1,7 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import { ReactChatWidget } from "./ReactChatWidget";
-import { ReactChatWidgetDarkTheme } from "../ReactChatWidgetDarkTheme";
 
-// todo akicha: the Default story should have the light theme in Storybook UI
+// todo akicha: add a toggle to toggle the dark class on the html element to preview the dark theme
 const meta: Meta<typeof ReactChatWidget> = {
   component: ReactChatWidget,
   title: "ReactChatWidget",
@@ -10,23 +9,15 @@ const meta: Meta<typeof ReactChatWidget> = {
   args: {
     className: "relative translate-x-0 translate-y-0 inset-0 mx-auto",
   },
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
 };
 
 export default meta;
 
-type Story = StoryObj<typeof ReactChatWidget>;
-
 export const Default = {
   args: {},
-};
-
-export const DarkTheme: Story = {
-  args: {},
-  decorators: [
-    (Story) => (
-      <ReactChatWidgetDarkTheme>
-        <Story />
-      </ReactChatWidgetDarkTheme>
-    ),
-  ],
 };
