aa3935cc2f5ad1e11e94f4dab23594ef8e272d34
Andrii Kycha <acousticdesk@gmail.com>
2024-11-26 09:47:08 -0700

    remove a redundant todo

--- src/components/ReactChatWidget/ReactChatbotWidget.tsx
+++ src/components/ReactChatWidget/ReactChatbotWidget.tsx
@@ -14,8 +14,6 @@ interface ReactChatbotWidgetProps {
   className?: string;
 }
 
-// todo akicha: remove tailwind and user styled-components instead
-
 // todo akicha: add the prefix to the tailwing classes!
 // todo akicha: should be cross-browser compatible
 // todo akicha: check the bundle size
