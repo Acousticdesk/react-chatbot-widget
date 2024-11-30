b9295222bab678418d697e887e5527a6b10183df
Acousticdesk <Acousticdesk@users.noreply.github.com>
2024-11-30 01:20:27 +0000

    deploy: 4b4263afd848e53966dd7ef639338d0ed5cb2b82

---
+++ assets/ReactChatbotWidget.stories-DpDc8zpV.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{c as p,R as u}from"./ReactChatbotWidget-DxvVknja.js";import"./index-DJO9vBfz.js";/* empty css              *//**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=p("Moon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);function g({children:a}){const i=h=>{const l=h.target.checked;document.documentElement.classList.toggle("dark",l)};return e.jsxs("div",{children:[e.jsxs("label",{className:"cw-text-white cw-flex cw-items-center cw-mb-4",children:[e.jsx("input",{type:"checkbox",onChange:i}),e.jsxs("span",{className:"cw-ml-2 cw-text-white cw-flex cw-items-center",children:[e.jsx(x,{className:"cw-mr-2"}),"Dark Theme"]})]}),a]})}const f={component:u,title:"ReactChatbotWidget",tags:["autodocs"],args:{className:"cw-mx-auto"}},t={args:{}},r={args:{},decorators:[a=>e.jsx(g,{children:e.jsx(a,{})})]};var s,c,o;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(c=t.parameters)==null?void 0:c.docs)==null?void 0:o.source}}};var n,m,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  decorators: [Story => {
+    return <WithDarkTheme>
+          <Story />
+        </WithDarkTheme>;
+  }]
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const T=["Default","DarkTheme"];export{r as DarkTheme,t as Default,T as __namedExportsOrder,f as default};
--- react-chatbot-widget.d.ts
+++ react-chatbot-widget.d.ts
@@ -1,9 +1,12 @@
+import { Dispatch } from 'react';
 import { JSX as JSX_2 } from 'react/jsx-runtime';
+import { SetStateAction } from 'react';
 
-export declare function ReactChatbotWidget({ className }: ReactChatbotWidgetProps): JSX_2.Element;
+export declare function ReactChatbotWidget({ className, setHeaderElement, }: ReactChatbotWidgetProps): JSX_2.Element;
 
 declare interface ReactChatbotWidgetProps {
     className?: string;
+    setHeaderElement?: Dispatch<SetStateAction<HTMLElement | null | undefined>>;
 }
 
 export { }
