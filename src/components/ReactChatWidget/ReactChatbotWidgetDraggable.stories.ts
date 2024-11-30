4b4263afd848e53966dd7ef639338d0ed5cb2b82
Andrii Kycha <acousticdesk@gmail.com>
2024-11-29 18:19:39 -0700

    add a todo

--- src/components/ReactChatWidget/ReactChatbotWidgetDraggable.stories.ts
+++ src/components/ReactChatWidget/ReactChatbotWidgetDraggable.stories.ts
@@ -1,6 +1,7 @@
 import { Meta } from "@storybook/react";
 import { ReactChatbotWidgetDraggable } from "./ReactChatbotWidgetDraggable";
 
+// todo akicha: hide the docs tab for this story
 const meta: Meta<typeof ReactChatbotWidgetDraggable> = {
   component: ReactChatbotWidgetDraggable,
   title: "ReactChatbotWidgetDraggable",
