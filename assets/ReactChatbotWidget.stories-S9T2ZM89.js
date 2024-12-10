1d61bd1925142305bcea670b6ce3759da63fb5e3
Acousticdesk <Acousticdesk@users.noreply.github.com>
2024-12-10 14:27:53 +0000

    deploy: c8cce60cefefb917a35d5faec9d80a272aa2c636

---
+++ assets/ReactChatbotWidget.stories-S9T2ZM89.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{R as p}from"./ReactChatbotWidget-CR5UogI1.js";import{c as u}from"./ReachChatbotWidgetDrawer-BpTpgo8j.js";import"./index-DJO9vBfz.js";/* empty css              *//**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=u("Moon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);function g({children:a}){const i=h=>{const l=h.target.checked;document.documentElement.classList.toggle("dark",l)};return e.jsxs("div",{children:[e.jsxs("label",{className:"cw-text-white cw-flex cw-items-center cw-mb-4",children:[e.jsx("input",{type:"checkbox",onChange:i}),e.jsxs("span",{className:"cw-ml-2 cw-text-white cw-flex cw-items-center",children:[e.jsx(x,{className:"cw-mr-2"}),"Dark Theme"]})]}),a]})}const T={component:p,title:"ReactChatbotWidget",tags:["autodocs"],args:{className:"cw-mx-auto"}},t={args:{}},r={args:{},decorators:[a=>e.jsx(g,{children:e.jsx(a,{})})]};var s,o,c;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(c=(o=t.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var n,m,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  decorators: [Story => {
+    return <WithDarkTheme>
+          <Story />
+        </WithDarkTheme>;
+  }]
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const b=["Default","DarkTheme"];export{r as DarkTheme,t as Default,b as __namedExportsOrder,T as default};
